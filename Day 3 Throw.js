a4cdc0631dc3bff51bd8ceed585d4ac19f09c0dd
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2022-12-13 18:52:01 +0600

    Day 3 Throw

---
+++ Day 3 Throw.js
@@ -0,0 +1,15 @@
+function isPositive(a) {
+    let result = 'YES';
+  
+    if (a < 0) {
+      throw { message: 'Negative Error' };
+    } else if (a === 0) {
+      throw { message: 'Zero Error' };
+    }
+  
+    return result;
+}
+
+isPositive(-1)
+isPositive(0)
+isPositive(1)
